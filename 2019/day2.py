14de2a4b5592678515e27ac425a98b1c0c49dcf1
Anh Le <quanganh@infinitelambda.com>
2021-08-31 09:26:52 +0700

    solution for part 2 of day 2

--- 2019/day2.py
+++ 2019/day2.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 with open('2019/day2.txt', 'r') as f:
     line = f.readline().rstrip()
 
@@ -15,16 +12,22 @@ class Codes:
     def program2(self, pos):
         self.codes[self.codes[pos+3]] = self.codes[self.codes[pos+1]] * self.codes[self.codes[pos+2]]
 
-seq = Codes(map(int, line.split(',')))
 
-i = 0
-seq.codes[1] = 12
-seq.codes[2] = 2
-while i < seq.len and seq.codes[i] != 99:
-    if seq.codes[i] == 1:
-        seq.program1(i)
-        i+=4
-    elif seq.codes[i] == 2:
-        seq.program2(i)
-        i+=4
-print(seq.codes[0])
+try_pairs = [(noun, verb) for noun in range(100) for verb in range(100)]
+for noun, verb in try_pairs:
+    seq = Codes(map(int, line.split(',')))
+    seq.codes[1] = noun
+    seq.codes[2] = verb
+    i = 0
+    while i < seq.len and seq.codes[i] != 99:
+        if seq.codes[i] == 1:
+            seq.program1(i)
+            i+=4
+        elif seq.codes[i] == 2:
+            seq.program2(i)
+            i+=4
+        else:
+            raise ValueError(f'Stuck in infinite loop at i = {i}, code = {seq.codes[1]}')
+    if seq.codes[0] == 19690720:
+        print(100*noun+verb)
+        break
