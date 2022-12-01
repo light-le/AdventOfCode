bd71dd49ec025547ce46f1372e7a840669f69277
Quang Anh <quanganh@infinitelambda.com>
2022-12-01 12:47:39 +0700

    init 2022 files

--- 2016/day22.py
+++ 2016/day22.py
@@ -1,11 +1,37 @@
 
+from dataclasses import dataclass
+from itertools import permutations
+from subprocess import list2cmdline
+from typing import List
 from utils import AdventSession, extract_year_day_from_path
 
 session = AdventSession(**extract_year_day_from_path(__file__))
 
+@dataclass
+class Node:
+    filesystem: str
+    size: int
+    used: int
+    avail: int
+    used_percent: int
+    
+    @classmethod
+    def parse_node(cls, data: str) -> object:
+        file, size, used, avail, usedp = data.split()
+        return cls(file,
+                   int(size.replace('T', '')),
+                   int(used.replace('T', '')),
+                   int(avail.replace('T', '')),
+                   int(usedp.replace('%', '')))
+    
+
 @session.submit_result(level=1)
-def solve_part1(inp):
-    pass
+def solve_part1(nodes: List[Node]):
+    viables = 0
+    for node1, node2 in permutations(nodes, 2):
+        if node1.used > 0 and node1.used <= node2.avail:
+            viables += 1
+    return viables
 
 @session.submit_result(level=2)
 def solve_part2(inp):
@@ -13,8 +39,10 @@ def solve_part2(inp):
 
 
 if __name__ == '__main__':
-    inp = session.read_input().split('\n')
+    inp = [i for i in session.read_input().split('\n') if i]
     
-    solve_part1(inp)
+    nodes = list(map(Node.parse_node, inp[2:]))
+    
+    solve_part1(nodes)
     
     solve_part2(inp)
