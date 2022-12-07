cbbf17d0057a967dc8821c7d9f850801fcdb0604
Quang Anh <quanganh@infinitelambda.com>
2022-12-07 09:36:52 +0700

    day 6 2022 completed

--- 2022/day6.py
+++ 2022/day6.py
@@ -3,17 +3,35 @@ from utils import AdventSession, extract_year_day_from_path
 
 session = AdventSession(**extract_year_day_from_path(__file__))
 
-@session.submit_result(level=1)
+@session.submit_result(level=1, tests=[
+    ({'inp': 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'}, 7),
+    ({'inp': 'bvwbjplbgvbhsrlpgdmjqwftvncz'}, 5),
+    ({'inp': 'nppdvjthqldpwncqszvftbrmjlhg'}, 6),
+    ({'inp': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'}, 10),
+    ({'inp': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'}, 11),
+])
 def solve_part1(inp):
-    pass
+    
+    for c in range(len(inp) - 4):
+        if len(set(inp[c:c+4])) == 4:
+            return c+4
 
-@session.submit_result(level=2)
+@session.submit_result(level=2, tests=[
+    ({'inp': 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'}, 19),
+    ({'inp': 'bvwbjplbgvbhsrlpgdmjqwftvncz'}, 23),
+    ({'inp': 'nppdvjthqldpwncqszvftbrmjlhg'}, 23),
+    ({'inp': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'}, 29),
+    ({'inp': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'}, 26),
+])
 def solve_part2(inp):
-    pass
+    for c in range(len(inp) - 14):
+        if len(set(inp[c:c+14])) == 14:
+            return c+14
+    
 
 
 if __name__ == '__main__':
-    inp = session.read_input().split('\n')
+    inp = session.read_input()
     
     solve_part1(inp)
     
